6bbdb55bf5eafd52dcbc42f315229b77afbc7d52
Hugo Martínez <hmartinez.lagos@gmail.com>
2017-04-19 16:51:38 -0300

    Muestra ciclo de vida con Toast
    Se muestra ciclo de vida con toast

--- Introduccion/app/src/main/java/cl/nextu/introduccinfragmentosyvistas/Ej1Fragmento.java
+++ Introduccion/app/src/main/java/cl/nextu/introduccinfragmentosyvistas/Ej1Fragmento.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 /**
  * Created by Admin on 19-04-2017.
@@ -15,25 +16,27 @@ import android.view.ViewGroup;
 
 public class Ej1Fragmento extends Fragment {
     private final String MENSAJE = "mensaje";
-
+    Context context = getActivity();
+    int duration = Toast.LENGTH_SHORT;
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
+        Toast.makeText( getActivity(), "onAttach", Toast.LENGTH_SHORT).show();
+       // Log.wtf(MENSAJE,"onAtach");
     }
-
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.wtf(MENSAJE, "oNCreate");
-
-
+       // Log.wtf(MENSAJE, "oNCreate");
+        Toast.makeText( getActivity(), "onCreate", Toast.LENGTH_SHORT).show();
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.ej1_fragmento, container, false);
-        Log.wtf(MENSAJE, "oNCreateView");
+       // Log.wtf(MENSAJE, "oNCreateView");
+        Toast.makeText( getActivity(), "onCreateView", Toast.LENGTH_SHORT).show();
         return view;
 
 
@@ -43,49 +46,58 @@ public class Ej1Fragmento extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        Log.wtf(MENSAJE, "onActivityCreated");
+      //  Log.wtf(MENSAJE, "onActivityCreated");
+        Toast.makeText( getActivity(), "onActivityCreated", Toast.LENGTH_SHORT).show();
+
     }
 
     @Override
     public void onStart() {
         super.onStart();
-        Log.wtf(MENSAJE, "onStart");
+        //Log.wtf(MENSAJE, "onStart");
+        Toast.makeText( getActivity(), "onStart", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        Log.wtf(MENSAJE, "onResume");
+       // Log.wtf(MENSAJE, "onResume");
+        Toast.makeText( getActivity(), "onResume", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onPause() {
         super.onPause();
-        Log.wtf(MENSAJE, "onPause");
+        //Log.wtf(MENSAJE, "onPause");
+        Toast.makeText( getActivity(), "onPause", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onStop() {
         super.onStop();
-        Log.wtf(MENSAJE, "onStop");
+        //Log.wtf(MENSAJE, "onStop");
+        Toast.makeText( getActivity(), "onAttach", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-        Log.wtf(MENSAJE, "onDestryView");
+        //Log.wtf(MENSAJE, "onDestryView");
+        Toast.makeText( getActivity(), "onDetryView", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        Log.wtf(MENSAJE, "onDestroy");
+       // Log.wtf(MENSAJE, "onDestroy");
+        Toast.makeText( getActivity(), "onDestroy", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
-        Log.wtf(MENSAJE, "onDetach");
+        //Log.wtf(MENSAJE, "onDetach");
+        Toast.makeText( getActivity(), "onDetach", Toast.LENGTH_SHORT).show();
     }
 
 
